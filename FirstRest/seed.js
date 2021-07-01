8b55bfeeecd65805f63c11d5f9b1a5c8832f02f6
apple <melvinfernando175@gmail.com>
2021-07-01 23:57:33 +0530

    improved images

--- FirstRest/seed.js
+++ FirstRest/seed.js
@@ -15,7 +15,7 @@ const Products = [
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "men's clothing",
     image:
-      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1624300401/ovrwihimjyaujjfceegx.png",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1624612507/5_bpueiu.jpg",
   },
   {
     name: "Mens Casual Premium Slim Fit T-Shirts",
@@ -24,7 +24,7 @@ const Products = [
       "Slim-fitting style, contrast raglan long sleeve, three-button henley placket.",
     category: "men's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162543/Group_2_vux8bx.png",
   },
   {
     name: "Mens Cotton Jacket",
@@ -33,7 +33,7 @@ const Products = [
       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors.",
     category: "men's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625163109/3_xfvsgh.png",
   },
   {
     name: "Mens Casual Slim Fit",
@@ -42,7 +42,7 @@ const Products = [
       "The color could be slightly different between on the screen and in practice.",
     category: "men's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162546/Group_18_tz7kiu.png",
   },
   {
     name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
@@ -51,7 +51,7 @@ const Products = [
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "men's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1624612507/5_bpueiu.jpg",
   },
   {
     name: "Solid Gold Petite Micropave",
@@ -60,7 +60,7 @@ const Products = [
       "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States.",
     category: "jewelery",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162545/Group_15_hf3ghe.png",
   },
   {
     name: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
@@ -69,7 +69,7 @@ const Products = [
       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl.",
     category: "jewelery",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162546/Group_17_i3w5ua.png",
   },
   {
     name: "White Gold Plated Princess",
@@ -78,7 +78,7 @@ const Products = [
       "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement.",
     category: "jewellery",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162544/Group_1_gwe2xm.png",
   },
   {
     name: "Pierced Owl Rose Gold Plated Stainless Steel Double",
@@ -87,7 +87,7 @@ const Products = [
       "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless ",
     category: "jewellery",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162542/Group_3_uw1kg5.png",
   },
   {
     name: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s.",
@@ -96,7 +96,7 @@ const Products = [
       "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive.",
     category: "electronics",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162544/Group_7_riklcn.png",
   },
   {
     name: "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
@@ -105,7 +105,7 @@ const Products = [
       "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance.",
     category: "electronics",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162545/Group_14_z1bwcw.png",
   },
   {
     name: "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive.",
@@ -114,7 +114,7 @@ const Products = [
       "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
     category: "electronics",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162546/Group_16_pziyyk.png",
   },
   {
     name: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin.",
@@ -123,7 +123,7 @@ const Products = [
       "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9.",
     category: "electronics",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162545/Group_8_jti3el.png",
   },
   {
     name: "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9.",
@@ -132,16 +132,16 @@ const Products = [
       "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support.",
     category: "electronics",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162546/Group_11_aduqwn.png",
   },
   {
     name: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
     price: 1000,
     description:
-      "ote:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester;",
+      "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester;",
     category: "men's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg ",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162545/Group_10_xteq02.png",
   },
   {
     name: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
@@ -150,7 +150,7 @@ const Products = [
       "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON.",
     category: "women's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162544/Group_6_mkr9tp.png",
   },
   {
     name: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
@@ -159,7 +159,7 @@ const Products = [
       "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat.",
     category: "women's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162547/Group_19_dtario.png",
   },
   {
     name: "MBJ Women's Solid Short Sleeve Boat Neck V",
@@ -168,7 +168,7 @@ const Products = [
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "women's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162546/Group_20_thgynu.png",
   },
   {
     name: "Opna Women's Short Sleeve Moisture",
@@ -177,16 +177,16 @@ const Products = [
       "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away.",
     category: "women's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162546/Group_21_ze9llr.png",
   },
   {
     name: "DANVOUY Womens T Shirt Casual Cotton Short.",
     price: 2905,
     description:
       "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch.",
-    category: "wom",
+    category: "women's clothing",
     image:
-      "https://pyxis.nymag.com/v1/imgs/853/030/fa9bfcf1f90e2fc68cd8a6fddb282fd1b6-men-packaged-dry-crew-neck.2x.rsquare.w600.jpg",
+      "https://res.cloudinary.com/dh0f4rsde/image/upload/v1625162545/Group_12_t49f5p.png",
   },
 ];
 
