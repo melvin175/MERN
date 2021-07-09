eb1e58146ac4a51f3b5e2c4fe581e56764f309ba
apple <melvinfernando175@gmail.com>
2021-07-10 00:44:37 +0530

    features on database

--- FirstRest/controller/userController.js
+++ FirstRest/controller/userController.js
@@ -1,5 +1,31 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require("express");
+const User = require("../model/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already Exists");
+  }
+
+  const user = await User.create()({
+    username,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      pic: user.pic,
+    });
+  }
 
   res.json({
     username,
@@ -7,6 +33,6 @@ const registerUser = async (req, res) => {
     password,
     pic,
   });
-};
+});
 
 module.exports = { registerUser };
--- FirstRest/model/userModel.js
+++ FirstRest/model/userModel.js
@@ -1,23 +1,30 @@
 var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-var userModel = new Schema({
-  username: {
-    type: String,
-    required: true,
+var userModel = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default: "https://image.flaticon.com/icons/png/512/1077/1077114.png",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  pic: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = userModel;
